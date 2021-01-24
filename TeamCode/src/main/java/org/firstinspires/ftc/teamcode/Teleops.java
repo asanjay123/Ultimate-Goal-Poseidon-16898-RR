73d4cf4a04b775dc8f9ee1e8825a65e56dfcd32f
asanjay123 <achintya_s@outlook.com>
2021-01-23 17:10:57 -0800

    FIrst push

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleops.java
@@ -0,0 +1,209 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Teleops", group = "")
+public class Teleops extends OpMode {
+
+    double drive;   // Power for forward and back motion
+    double strafe;  // Power for left and right motion
+    double rotate;  // Power for rotating the robot
+    DcMotor frontLeft;
+    DcMotor backLeft;
+    DcMotor frontRight;
+    DcMotor backRight;
+    DcMotor intake;
+    DcMotor wobble;
+//    DcMotor shooter;
+    //
+//    Servo flicker;
+    Servo claw;
+//    Servo servo2;
+//    Servo hookRight;
+//    Servo hookLeft;
+//    double position;
+
+    double nitro;
+
+    @Override
+    public void init(){
+        frontLeft = hardwareMap.dcMotor.get("frontleft");
+        backLeft = hardwareMap.dcMotor.get("backleft");
+        frontRight = hardwareMap.dcMotor.get("frontright");
+        backRight = hardwareMap.dcMotor.get("backright");
+
+        intake = hardwareMap.dcMotor.get("ring");
+        wobble = hardwareMap.dcMotor.get("wobble");
+//        flicker = hardwareMap.servo.get("pusher");
+        claw = hardwareMap.servo.get("clamp");
+//        shooter = hardwareMap.dcMotor.get("flywheel");
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        nitro = 2.0;
+//        servo.setPosition(0.14);
+//     //   servo.setPosition(position);
+//        servo2.setPosition(.6);
+//        //servo2.setPosition(servo1.getPosition()+0.5);
+//
+//        hookLeft.setPosition(0.3);
+//        hookRight.setPosition(0.3);
+
+
+//
+//        position = servo;
+//        servo.setPosition(position);
+
+
+    }
+
+    @Override
+    public void loop() {
+
+
+
+        //Intake controls
+        if (gamepad1.x) {
+            intake.setPower(1);
+        }
+        if (gamepad1.y) {
+            intake.setPower(0);
+        }
+        if (gamepad1.a) {
+            intake.setPower(-1);
+        }
+
+        //Drive controls
+        drive = -gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        rotate = -gamepad1.right_stick_x;
+
+        double frontLeftPower = drive + strafe - rotate;
+        double backLeftPower = drive - strafe - rotate;
+        double frontRightPower = drive - strafe + rotate;
+        double backRightPower = drive + strafe + rotate;
+
+        frontLeft.setPower(frontLeftPower/nitro);
+        backLeft.setPower(backLeftPower/nitro);
+        frontRight.setPower(frontRightPower/nitro);
+        backRight.setPower(backRightPower/nitro);
+
+        //Nitro controls
+        if (gamepad1.a){
+            nitro = 1;
+        }
+        else if (gamepad1.b){
+            nitro = 2;
+        }
+
+        if (gamepad1.left_trigger > 0){
+            nitro = 4;
+        }else if (gamepad1.right_trigger > 0) {
+            nitro = 1;
+        }
+        else{
+            nitro = 2;
+        }
+
+        //Wobble Arm controls
+        if (gamepad2.right_bumper){
+            wobble.setPower(0.6);
+        }
+        else if (gamepad2.left_bumper) {
+            wobble.setPower(-0.6);
+        }
+        else {
+            wobble.setPower(-0.05);
+        }
+
+//        Claw controls
+        if (gamepad2.x)
+            claw.setPosition(0.88);
+        if (gamepad2.y)
+            claw.setPosition(0.5);
+//
+//        //Shooter controls
+//        if (gamepad2.x) {
+//            shooter.setPower(1);
+//        }
+//        if (gamepad2.y) {
+//            shooter.setPower(0);
+//        }
+//        if (gamepad1.a) {
+//            shooter.setPower(-1);
+//        }
+//
+//        //Flicker settings
+//        if (gamepad2.right_trigger > 0) {
+//            flicker.setPosition(0.6);
+//        }
+//        else {
+//            flicker.setPosition(0.17);
+//        }
+//
+
+
+//        try {
+//            servo.setPosition(position);
+//        }
+//        catch(Exception e) {
+//            int x = 5;
+//        }
+//
+//        if (gamepad2.dpad_up){
+//            try{
+//                servo1.setPosition(servo1.getPosition() + .01);
+//            }catch(Exception e){
+//                int x = 5;
+//            }
+//        }
+//
+//        if (gamepad2.dpad_down){
+//            try{
+//                servo1.setPosition(servo1.getPosition() - .01);
+//            }catch(Exception e){
+//                int x = 5;
+//            }
+//        }
+//
+//        if (gamepad1.left_stick_button){
+//            servo1.setPosition(0);
+//        }
+//        if (gamepad1.right_stick_button){
+//            servo1.setPosition(.6);
+//        }
+//
+//       // servo2.setPosition(0);
+//
+//        if (gamepad2.dpad_left){
+//            try{
+//                servo2.setPosition(0);
+//            }catch(Exception e){
+//                int x = 5;
+//            }
+//
+//        }
+//        if (gamepad2.dpad_right){
+//            try{
+//                servo2.setPosition(0);
+//            }catch(Exception e){
+//
+//                int x = 5;
+//            }
+//        }
+
+        telemetry.addData("Nitro: ", (nitro == 1) ? "High":( (nitro==2) ? "Medium":"Low"));
+//        telemetry.addData("Twisty Position: ", servo2.getPosition());
+        telemetry.update();
+    }
+}
